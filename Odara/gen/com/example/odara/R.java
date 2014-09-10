05ac75025387c3adebde9ebd18390369a236d5b6
talitabac <talitabac@gmail.com>
2014-09-09 21:31:49 -0300

    telas

--- Odara/gen/com/example/odara/R.java
+++ Odara/gen/com/example/odara/R.java
@@ -1197,7 +1197,7 @@ containing a value of this type.
         public static final int abc_simple_decor=0x7f030017;
         public static final int activity_main=0x7f030018;
         public static final int criar_festa=0x7f030019;
-        public static final int spash_inicial=0x7f03001a;
+        public static final int login=0x7f03001a;
         public static final int support_simple_spinner_dropdown_item=0x7f03001b;
     }
     public static final class menu {
