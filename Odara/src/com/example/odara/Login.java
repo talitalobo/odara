b036bf21cfbbf7fb6b49930ff9562112e78c47f6
talitabac <talitabac@gmail.com>
2014-09-09 22:53:33 -0300

    correcoes

--- Odara/src/com/example/odara/Login.java
+++ Odara/src/com/example/odara/Login.java
@@ -13,7 +13,7 @@ public class Login extends ActionBarActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		setContentView(R.layout.login);
+		setContentView(R.layout.login_fb);
 
 		/*new Handler().postDelayed(new Runnable() {
 			@Override
